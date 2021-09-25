dd1f31d56f5ece3b9dae25789ea295032a8f5969
Stiven <starias@ucm.es>
2021-09-25 18:42:47 +0200

    Update: cálculos y métodos para el desciente de gradiente. Falta la gŕafica

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Practica1
-Práctica 1 de la asignatura de Aprendizaje Automático y Minería de datos del grado de Desarrollo de Videojuegos de la Universidad Complutense de Madrid. Consiste en solucionar dos problemas sobre **Regresión lineal** detallados en [**enunciado_p1.pdf**](https://github.com/Aprendizaje-Automatico-2021/Practica1/blob/main/enunciado_p1.pdf)
+Práctica 1 de la asignatura de Aprendizaje Automático y Minería de datos del grado de Desarrollo de Videojuegos de la Universidad Complutense de Madrid. Consiste en solucionar dos problemas sobre **Regresión lineal** detallados en [**enunciado.pdf**](https://github.com/Aprendizaje-Automatico-2021/Practica1/blob/main/enunciado.pdf)
--- src/solucion_1.py
+++ src/solucion_1.py
@@ -1,64 +1,92 @@
 import numpy as np
 from pandas.io.parsers import read_csv
+import matplotlib.pyplot as plt
 
-def function_J(m, valores, theta_0, theta_1):
+def function_J(m, X, Y, theta0, theta1):
+    """
+    Calculate the function J(theta)
+    """
     sum = 0
     for i in range(m):
-        sum = sum + (valores[i][1] ** 2)
+        sum = sum + (hipotesis(X[i], theta0, theta1) - Y[i]) ** 2
 
-    return  (1 / 2 * m) * sum
+    result = (1 / 2 * m) * sum
+    return  result.astype(float)
 
-def hipotesis(x, theta_0, theta_1):
+def hipotesis(x, theta0, theta1):
     """
-    Función de la hipotésis
+    Calculate the hypothesis function h(x) = theta0 + theta1 * x
     """
-    return theta_0 + theta_1 * x
+    result = theta0 + theta1 * x
+    return result.astype(float)
 
-def diff(x, y, theta_0, theta_1):
+def diff(x, y, theta0, theta1):
     """
         return h(xi) - yi 
     """
-    return hipotesis(x, theta_0, theta_1) - y
+    result = hipotesis(x, theta0, theta1) - y
+    return result.astype(float)
 
-def new_theta_0(m, valores, theta_0, theta_1, alpha):
+def new_theta_0(m, X, Y, theta0, theta1, alpha):
+    """
+    Calculate the new value of theta0
+    """
     sum = 0
     for i in range(m):
-        sum = sum + diff(valores[i][0], valores[i][1], theta_0, theta_1)
+        sum = sum + diff(X[i], Y[i], theta0, theta1)
 
-    return  theta_0 - (alpha / m) * sum
+    result = theta0 - (alpha / m) * sum
+    return  result.astype(float)
 
-def new_theta_1(m, valores, theta_0, theta_1, alpha):
+def new_theta_1(m, X, Y, theta0, theta1, alpha):
+    """
+    Calculate the new value of theta1
+    """
     sum = 0
     for i in range(m):
-        sum = sum + diff(valores[i][0], valores[i][1], theta_0, theta_1) * valores[i][0]
+        sum = sum + diff(X[i], Y[i], theta0, theta1) * X[i]
 
-    return  theta_1 - (alpha / m) * sum
+    result = theta1 - (alpha / m) * sum 
+    return  result.astype(float)
+
+def read_data():
+    """
+    Read dthe data of the file and return the result as a float
+    """
+    valores = read_csv("./ex1data1.csv", header=None).to_numpy()
+    return valores.astype(float)
 
 def gradient():
-    valores = read_csv("./p1/ex1data1.csv" , header=None).to_numpy()
-    m  = len(valores)
+    """
+    Main function to calculate the descent of the gradient
+    """
+    # Valores de muestra
+    valores = read_data()
+    X = valores[:, 0]
+    Y = valores[:, 1]
+    m  = len(X)
     
-    theta_0 = 0.0
-    theta_1 = 0.0
-    alpha = 0.1
+    theta0 = 0.0
+    theta1 = 0.0
+    alpha = 0.01
+    # Current value of the function J(theta)
     curr_J = 0
-    min = 1000000
 
     i = 0
+    # Calculate the new cost of J function
+    curr_J = function_J(m, X, Y, theta0, theta1)
+    print("Para theta0 = {} // theta1 = {}: J = {}".format(theta0, theta1, curr_J))
+
     for i in range(1500):
-        # Calculate the new cost of J function
-        curr_J = function_J(m, valores, theta_0, theta_1)
-        if curr_J < min: 
-            min = curr_J
-
         # Calculate the new values to theta_0 and theta_1
-        temp_0  = new_theta_0(m, valores, theta_0, theta_1, alpha)
-        temp_1  = new_theta_1(m, valores, theta_0, theta_1, alpha)
-        theta_0 = temp_0
-        theta_1 = temp_1
+        temp0  = new_theta_0(m, X, Y, theta0, theta1, alpha)
+        temp1  = new_theta_1(m, X, Y, theta0, theta1, alpha)
+        theta0 = temp0
+        theta1 = temp1
 
-    print(curr_J)
+        # Calculate the new cost of J function
+        curr_J = function_J(m, X, Y, theta0, theta1)
+        print("Para theta0 = {} // theta1 = {}: J = {}".format(theta0, theta1, curr_J))
 
-    # Graph drawing
-
+# main
 gradient()
